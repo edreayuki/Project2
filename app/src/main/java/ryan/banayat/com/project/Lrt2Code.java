4f7a46688b370add4daa0a72ca84afcae576bf78
edreayuki <edreayuki.yumul.iics@ust.edu.ph>
2018-12-09 20:45:03 +0800

    LRT2 done

--- app/src/main/java/ryan/banayat/com/project/Lrt2Code.java
+++ app/src/main/java/ryan/banayat/com/project/Lrt2Code.java
@@ -32,32 +32,75 @@ public class Lrt2Code extends AppCompatActivity {
             case R.id.img1:
             case R.id.txt1:
                 i.putExtra("station_name", "Santolan");
-                i.putExtra("latitude", "14.6576");
-                i.putExtra("longitude", "121.0211");
+                i.putExtra("latitude", "14.6215");
+                i.putExtra("longitude", "121.0843");
                 break;
 
             case R.id.txt2:
             case R.id.img2:
                 i.putExtra("station_name", "Katipunan");
-                i.putExtra("latitude", "14.6574");
-                i.putExtra("longitude", "121.0032");
+                i.putExtra("latitude", "14.6314");
+                i.putExtra("longitude", "121.0734");
                 break;
 
 
             case R.id.txt3:
             case R.id.img3:
                 i.putExtra("station_name", "Anonas");
-                i.putExtra("latitude", "14.6543");
-                i.putExtra("longitude", "120.9838");
+                i.putExtra("latitude", "14.6279");
+                i.putExtra("longitude", "121.0642");
                 break;
 
             case R.id.txt4:
             case R.id.img4:
                 i.putExtra("station_name", "Araneta Center-Cubao");
-                i.putExtra("latitude", "14.6543");
-                i.putExtra("longitude", "120.9838");
+                i.putExtra("latitude", "14.6228");
+                i.putExtra("longitude", "121.0525");
+                break;
+            case R.id.txt5:
+            case R.id.img5:
+                i.putExtra("station_name", "Betty Go-Belmonte");
+                i.putExtra("latitude", "14.6186");
+                i.putExtra("longitude", "121.0426");
                 break;
 
+            case R.id.txt6:
+            case R.id.img6:
+                i.putExtra("station_name", "Gilmore");
+                i.putExtra("latitude", "14.6135");
+                i.putExtra("longitude", "121.0343");
+                break;
+
+            case R.id.txt7:
+            case R.id.img7:
+                i.putExtra("station_name", "J. Ruiz");
+                i.putExtra("latitude", "14.6105");
+                i.putExtra("longitude", "121.0266");
+                break;
+            case R.id.txt8:
+            case R.id.img8:
+                i.putExtra("station_name", "V. Mapa");
+                i.putExtra("latitude", "14.6042");
+                i.putExtra("longitude", "121.0172");
+                break;
+            case R.id.txt9:
+            case R.id.img9:
+                i.putExtra("station_name", "Pureza");
+                i.putExtra("latitude", "14.6017");
+                i.putExtra("longitude", "121.0049");
+                break;
+            case R.id.txt10:
+            case R.id.img10:
+                i.putExtra("station_name", "Legarda");
+                i.putExtra("latitude", "14.6010");
+                i.putExtra("longitude", "120.9922");
+                break;
+            case R.id.img11:
+            case R.id.txt11:
+                i.putExtra("station_name", "Recto");
+                i.putExtra("latitude", "14.6035");
+                i.putExtra("longitude", "120.9831");
+                break;
         }
 
         startActivity(i);
